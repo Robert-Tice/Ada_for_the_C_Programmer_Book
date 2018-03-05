db6df7f7a4ef21427842a2cb94e4ba77c07f8d75
Robert Tice <tice@adacore.com>
2018-03-05 16:43:10 -0500

    Generated gh-pages for commit 00b0e87 Author: Robert Tice <tice@adacore.com>
        Setting up for sphinx build.

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Ada for the C Programmer\u2019s documentation!"],titleterms:{ada:0,document:0,indic:0,programm:0,tabl:0,welcom:0}})
